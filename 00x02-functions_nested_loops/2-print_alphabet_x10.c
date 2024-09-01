e73a98a9ad8b90d2e597512e37d8a9ebf4f8fcff
niylii <nouhashahde@gmail.com>
2024-09-01 23:13:43 +0200

    prints alphabet 10 time

---
+++ 00x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints alphabet in lowercase
+* @t: the number of times.
+*
+* return : nothing 
+*/
+
+void	print_alphabet_x10(void)
+{
+	char	c;
+	int	t;
+
+	t = 1;
+	while (t < 11)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		t++;
+	}
+}
