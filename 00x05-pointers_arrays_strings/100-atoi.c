f573911b4f18b28d8dc3f851092194ec7ede2b7f
niylii <nouhashahde@gmail.com>
2024-09-13 00:03:39 +0200

    atoi[simpler]

--- 00x05-pointers_arrays_strings/100-atoi.c
+++ 00x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * signe - function to count the number of '-' signs.
- * @s: the string
- * Return: number of '-' signs
- */
-
-int signe(char *s)
-{
-	int i = 0, j = 0;
-
-	while (s[i])
-	{
-		if (s[i] == '-')
-		{
-			j++;
-		}
-	i++;
-	}
-	return (j);
-}
-
-/**
  * _atoi - function that convert a string to an integer.
  * @s : the string
  * Return: integer
@@ -29,27 +8,21 @@ int signe(char *s)
 
 int _atoi(char *s)
 {
-	int  si, ch = 0;
+	int si  = 1;
 	unsigned int r = 0;
 
-	si = signe(s);
-	while (*s && !((*s >= '0') && (*s <= '9')))
-	{
+	if (*s == '+' || *s == ' ')
 		s++;
+	if (*s == '-')
+	{
+		si *= -1;
 	}
-	while (*s)
+	while (*s++)
 	{
 		if ((*s >= '0') && (*s <= '9'))
 		{
-			ch = 1;
 			r = (r * 10) + (*s - '0');
 		}
-		else if (ch)
-			break;
-		s++;
 	}
-	if (si % 2 == 0)
-		return (r);
-	else
-		return (-r);
+	return (r * si);
 }
