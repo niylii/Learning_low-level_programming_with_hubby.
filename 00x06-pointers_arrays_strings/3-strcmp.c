ce617f4e64f4ae31e37a8e88a92ea37f66a76352
niylii <nouhashahde@gmail.com>
2024-09-26 14:15:33 +0200

    [fixed] comparing whole string now (direct calculation , no initializinggit add 3-strcmp.c )

--- 00x06-pointers_arrays_strings/3-strcmp.c
+++ 00x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int r = *s1 - *s2;
-
 	while (*s1 && *s2)
 	{
 		if (*s1 != *s2)
-			return (r);
+			return (*s1 - *s2);
 		s1++;
 		s2++;
 	}
-	return (r);
+	return (*s1 - *s2);
 }
