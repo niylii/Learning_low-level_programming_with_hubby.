466a80389ebf96cce08dc31df5d3c6d356d94849
niylii <nouhashahde@gmail.com>
2024-09-29 01:46:33 +0200

    sum of two diagonal matrix of integers

--- 00x07-even_pointers_arrays_strings/8-print_diagsums.c
+++ 00x07-even_pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * print_diagsums - a function that prints the sum of the two diagonals
 *	 of a square matrix of integers.
 * @a : a pointer to a string
-* @size : 
+* @size : the size
+* Return: none
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int d1 = 0, d2 = 0;
+
+	while (i < size)
+	{
+			d1 += a[(i * size) + i];
+			d2 += a[i * size + (size - 1 - i)];
+		i++;
+	}
+	printf("%d , %d\n", d1, d2);
+}
