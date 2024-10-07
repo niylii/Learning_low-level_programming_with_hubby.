1eda625756a27991ba6ece29a502f2c900f9e774
niylii <nouhashahde@gmail.com>
2024-10-08 00:38:36 +0200

    program that prints its name

---
+++ 00x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program.
+ * @argc : argument count.
+ * @argc : argument.
+ * Return: 0 (success).
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
