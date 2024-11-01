93ccc1cdf6c59d087e3f38085e92a21205292349
niylii <niylii@users.noreply.github.com>
2024-11-01 20:08:47 +0100

    the functions needed to do simple operations

---
+++ 00x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that gives the sum of two numbers
+ * @a : number
+ * @b : number
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that gives the diffrence of two numbers
+ * @a : number
+ * @b : number
+ * Return: diffrence of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that gives the product of two numbers
+ * @a : number
+ * @b : number
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that gives the result of the division two numbers
+ * @a : number
+ * @b : number
+ * Return: result of devision of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that gives the  remainder of the division
+ *  of two numbers
+ * @a : number
+ * @b : number
+ * Return:  remainder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
