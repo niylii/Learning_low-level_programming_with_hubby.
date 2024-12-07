5d0f976c6b63c805dba81c0f0f3e057d013d602e
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2024-12-07 12:56:06 +0100

    task-10 [update : made a better syntax]

--- 00x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 00x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp = *head, *tmp2;
 	unsigned int i = 0;
 
-	if (tmp == NULL)
+	if (!tmp)
 		return (-1);
 	if (index == 0)
 	{
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tmp = tmp->next;
 		i++;
 	}
-	if (tmp == NULL || tmp->next == NULL)
+	if (!tmp || !tmp->next)
 		return (-1);
 	tmp2 = tmp->next;
 	tmp->next = tmp2->next;
