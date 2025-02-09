9ea810f2da264523756c1da3ec97a6e40778f843
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2025-02-09 19:40:59 +0100

    task - 1

--- 00x17-doubly_linked_lists/1-dlistint_len.c
+++ 00x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 /**
- * print_dlistint - a function that returns the number
+ * dlistint_len - a function that returns the number
  *	 of the elements of a list
  * @h : a pointer to the list head
  * Return: the number of the nodes
  */
-
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int i = 0;
 	const dlistint_t *tmp = h;
